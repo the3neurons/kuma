f50e7975335c81a0870387fe7d77f7c3a072678a
opixelum <48217573+opixelum@users.noreply.github.com>
2025-04-17 20:34:53 +0200

    feat(text_extraction.py): differentiate senders

--- src/text_extraction.py
+++ src/text_extraction.py
@@ -1,21 +1,44 @@
 import boto3
+import json
+from typing import Any
 
-# Document
-documentName = "../test.jpg"
+# # Document
+# documentName = "../test.png"
+#
+# # Read document content
+# with open(documentName, 'rb') as document:
+#     imageBytes = bytearray(document.read())
+#
+# # Amazon Textract client
+# textract = boto3.client('textract')
+#
+# # Call Amazon Textract
+# response = textract.detect_document_text(Document={'Bytes': imageBytes})
+# with open("response.json", "w") as file:
+#     json.dump(response, file)
 
-# Read document content
-with open(documentName, 'rb') as document:
-    imageBytes = bytearray(document.read())
+with open("response.json", "r") as file:
+    response: dict[str, Any] = json.load(file)
 
-# Amazon Textract client
-textract = boto3.client('textract')
+# For SMS-like messages (imessage currently)
+# The recipient messages have the property "left" equal to 0.07 (near 0)
+# Our messages have this property equal or greater than 0.3
+# For Discord, reddit, etc..., another strategy is needed
 
-# Call Amazon Textract
-response = textract.detect_document_text(Document={'Bytes': imageBytes})
+sender: int | None = None  # 0 = me, 1 = recipient
 
-print(response)
-
-# Print detected text
 for item in response["Blocks"]:
     if item["BlockType"] == "LINE":
-        print ('\033[94m' +  item["Text"] + '\033[0m')
+        if item["Geometry"]["BoundingBox"]["Left"] < 0.1:
+            if sender is None or sender == 0:
+                print("\nRecipient:")
+                sender = 1
+
+            print(item["Text"])
+
+        else:
+            if sender is None or sender == 1:
+                print("\nMe:")
+                sender = 0
+
+            print(item["Text"])
